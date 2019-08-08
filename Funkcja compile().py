14e05e8e1a78733ac2ba006f7e86539882f41840
Ad7siem <ad7siem@gmail.com>
2019-08-08 21:53:54 +0200

    zrobione ćwiczenia

--- Funkcja compile().py
+++ Funkcja compile().py
@@ -5,21 +5,57 @@ source = 'reportLine += 1'
 reportLine = 0
 
 start = time.time()
-for i in range(10000):
+for i in range(1000):
     exec(source)
 stop = time.time()
 time_not_compiled = stop - start
 
 start = time.time()
-sourceComplited = compile(source,'internal variablesource', 'exec')
+sourceComplited = compile(source, 'internal variablesource', 'exec')
 
-for i in range(10000):
+for i in range(1000):
     exec(sourceComplited)
 stop = time.time()
 time_compiled = stop - start
 
 print(time_not_compiled)
 print(time_compiled)
-print(time_not_compiled/time_compiled)
+print(time_not_compiled / time_compiled)
 
 print(reportLine)
+
+# ćwiczenia
+
+import math
+import time
+
+formulas_list = [
+    "abs(x**3 - x**0.5)",
+    "abs(math.sin(x) * x**2)"
+]
+
+argument_list = []
+for i in range(1000000):
+    argument_list.append(i / 10)
+    i += 1
+
+for formula in formulas_list:
+    results_list = []
+    print(formula)
+    start = time.time()
+    for x in argument_list:
+        results_list.append(eval(formula))
+    print('Minimalna wartość obliczeń wynosi:', min(results_list), ', a maksymalna to:', max(results_list))
+    stop = time.time()
+    print('Obliczenia trwały:', stop - start)
+
+for formula in formulas_list:
+    results_list = []
+    print(formula)
+    start = time.time()
+    compiled_formula = compile(formula, formula, 'eval')
+    for x in argument_list:
+        results_list.append(eval(compiled_formula))
+    print('Minimalna wartość obliczeń wynosi:', min(results_list), ', a maksymalna to:', max(results_list))
+    stop = time.time()
+    print('Obliczenia trwały:', stop - start)
