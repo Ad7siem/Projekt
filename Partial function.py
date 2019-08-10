9bdde711e9e87f74da45293e11c2b98c55199117
Ad7siem <ad7siem@gmail.com>
2019-08-10 17:12:43 +0200

    ćwiczenia zrobione

--- Partial function.py
+++ Partial function.py
@@ -1,15 +1,13 @@
-import smtplib
+"""import smtplib
+import functools
 
-def SendInfoEmail (user, password, mailFrom,mailTo,mailSubject,mailBody):
 
-    message2 = '''From: {} 
+def SendInfoEmail(user, password, mailFrom, mailTo, mailSubject, mailBody):
+    message2 = '''From: {}
 Subject: {}
 
 {}
-'''.format(mailFrom, mailSubject, mailBody)  # tu jest problem
-
-# gdy usunę klamre i mailBody mail zostaje wysłany.
-# Teraz skrypt niezadziala. Nie rozumiem co jest nie tak w debbugu ale wydawalo mi sie ze problem z wartoscia str
+'''.format(mailFrom, mailSubject, mailBody)  # encoding utf-8
 
     message = 'Sprawdzam'
 
@@ -25,20 +23,52 @@ Subject: {}
         print('Error sending email')
         return False
 
+
 mailFrom = 'Your automatic system'
 mailTo = ['Ad_siem@windowslive.com']
 mailSubject = 'Processing finished successfully'
-mailBody = '''
-Ja wiem, 
-Wiem czego chcesz, 
-wiem czego brak, 
-wiem co bys chcial, 
-a mozliwosci do tego brak
-'''
+mailBody = '''Dziala?
+Ja wiem,
+Wiem czego chcesz,
+wiem czego brak,
+wiem co bys chcial,
+a mozliwosci do tego brak'''  # encoding utf-8
 
-user = input('Wprowadź swojego gmaila: ')
+user = input('Wprowadź swojego @gmail.com:')
 password = input('Wprowadź hasło: ')
 
-SendInfoEmail(user,password,mailFrom,mailTo,mailSubject,mailBody)
+SendInfoEmailFromGmail = functools.partial(SendInfoEmail, user, password)
+
+SendInfoEmailFromGmail(mailFrom, mailTo, mailSubject, mailBody)
+
+# SendInfoEmail(user, password, mailFrom, mailTo, mailSubject, mailBody)
+"""
+
+# ćwiczenia
+
+import requests
+import os
+import functools
 
 
+def save_url_file(url, dir, file, msg):
+    print(msg.format(file))
+
+    r = requests.get(url, stream=True)
+    file_path = os.path.join(dir, file)
+
+    with open(file_path, "wb") as f:
+        f.write(r.content)
+
+
+save_url_to_dir = functools.partial(save_url_file, dir='D:/temp/', msg='Please wait: {}')
+
+
+url = 'http://mobilo24.eu/spis'
+file = 'spis.html'
+save_url_to_dir(url = url, file = file)
+
+url = 'https://www.mobilo24.eu/wp-content/uploads/2015/11/Mobilo_logo_kolko_512-565b1626v1_site_icon.png'
+dir = 'D:/temp/'
+file = 'logo.png'
+save_url_to_dir(url = url, file = file)
